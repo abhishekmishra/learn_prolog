a573831f4f2dcd694e4314e0376b11b53203d0d3
Abhishek Mishra <abhishekmishra3@gmail.com>
2018-11-25 12:56:34 +0530

    added few more

---
+++ country-capital-few.pl
@@ -0,0 +1,14 @@
+country('India').
+country('Sri Lanka').
+country('Bangladesh').
+country('Nepal').
+
+capital('India', 'New Delhi').
+capital('Sri Lanka', 'Colombo').
+capital('Bangladesh', 'Dhaka').
+capital('Nepal', 'Kathmandu').
+
+tld('India', in).
+tld('Sri Lanka', sl).
+tld('Bangladesh', bd).
+tld('Nepal', np).
