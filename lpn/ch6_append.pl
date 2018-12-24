b50e1f624d72f15f97d4c1650cabced37e14fba5
Abhishek Mishra <abhishekmishra3@yahoo.com>
2018-12-24 12:54:15 +0530

    added naiverev

--- lpn/ch6_append.pl
+++ lpn/ch6_append.pl
@@ -4,3 +4,7 @@ append([H|T],L2,[H|L3]) :- append(T,L2,L3).
 prefix(P, L):- append(P, _, L).
 suffix(S, L):- append(_, S, L).
 subList(SubL, L):- suffix(S, L), prefix(SubL, S).
+
+naiverev([],[]).
+naiverev([H|T],R):-
+    naiverev(T,RevT), append(RevT,[H],R).
